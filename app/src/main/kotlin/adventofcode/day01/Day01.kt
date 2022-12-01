75dd03cdfee9ede0a4e3608a774aea0d83036c40
Stefan Hering <shering@spotify.com>
2022-12-01 08:38:57 +0100

    Day 1

--- app/src/main/kotlin/adventofcode/day01/Day01.kt
+++ app/src/main/kotlin/adventofcode/day01/Day01.kt
@@ -3,10 +3,34 @@ package adventofcode.day01
 fun readInput(file: String) = Unit.javaClass.getResource(file)
         .readText()
         .lines()
-        .filter { it.isNotEmpty() }
+        .fold(mutableListOf(mutableListOf<Int>())) { acc, line ->
+            if (line.isBlank()) {
+                acc.add(mutableListOf())
+            } else {
+                acc.last().add(line.toInt())
+            }
+            acc
+        }
+
+typealias Elf = List<Int>
+
+fun part1(elves: List<Elf>) = elves
+        .map {
+            it.sum()
+        }.max()
+
+fun part2(elves: List<Elf>) = elves
+        .map {
+            it.sum()
+        }.sortedDescending()
+        .take(3)
+        .sum()
 
 fun main() {
     val input = readInput("/day01/input")
     val testinput = readInput("/day01/testinput")
+
+    println(part1(input))
+    println(part2(input))
 }
 
