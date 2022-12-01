56d7d5c6822842d0849b44222ad02edb8a600296
Stefan Hering <shering@spotify.com>
2022-12-01 08:43:37 +0100

    Cleanup Day 1

--- app/src/main/kotlin/adventofcode/day01/Day01.kt
+++ app/src/main/kotlin/adventofcode/day01/Day01.kt
@@ -1,36 +1,31 @@
 package adventofcode.day01
 
 fun readInput(file: String) = Unit.javaClass.getResource(file)
-        .readText()
-        .lines()
-        .fold(mutableListOf(mutableListOf<Int>())) { acc, line ->
-            if (line.isBlank()) {
-                acc.add(mutableListOf())
-            } else {
-                acc.last().add(line.toInt())
-            }
-            acc
-        }
+    .readText()
+    .split("\n\n")
+    .map {
+      it.lines().map { it.toInt() }
+    }
 
 typealias Elf = List<Int>
 
 fun part1(elves: List<Elf>) = elves
-        .map {
-            it.sum()
-        }.max()
+    .map {
+      it.sum()
+    }.max()
 
 fun part2(elves: List<Elf>) = elves
-        .map {
-            it.sum()
-        }.sortedDescending()
-        .take(3)
-        .sum()
+    .map {
+      it.sum()
+    }.sortedDescending()
+    .take(3)
+    .sum()
 
 fun main() {
-    val input = readInput("/day01/input")
-    val testinput = readInput("/day01/testinput")
+  val input = readInput("/day01/input")
+  val testinput = readInput("/day01/testinput")
 
-    println(part1(input))
-    println(part2(input))
+  println(part1(input))
+  println(part2(input))
 }
 
