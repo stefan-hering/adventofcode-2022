2d9ab142436711cc4419be3a7ff47b45b283669d
Stefan Hering <shering@spotify.com>
2022-12-04 10:49:32 +0100

    Day 3

--- app/src/main/kotlin/adventofcode/day03/Day03.kt
+++ app/src/main/kotlin/adventofcode/day03/Day03.kt
@@ -4,9 +4,43 @@ fun readInput(file: String) = Unit.javaClass.getResource(file)
     .readText()
     .lines()
     .filter { it.isNotEmpty() }
+    .map { it.toCharArray() }
+
+fun part1(rucksäcke: List<CharArray>): Int =
+    rucksäcke.map {
+      it.copyOfRange(0, it.size / 2) to it.copyOfRange(it.size / 2, it.size)
+    }.mapNotNull { r ->
+      r.first.find {
+        r.second.contains(it)
+      }
+    }.map {
+      priority(it)
+    }.sum()
+
+fun part2(rucksäcke: List<CharArray>) =
+    rucksäcke.windowed(3, step = 3)
+        .mapNotNull { elfGroup ->
+          elfGroup[0].filter {
+            elfGroup[1].contains(it)
+          }.find {
+            elfGroup[2].contains(it)
+          }
+        }
+        .map { priority(it) }
+        .sum()
+
+fun priority(item: Char) =
+    if (item.isUpperCase()) {
+      item.toInt() - 38
+    } else {
+      item.toInt() - 96
+    }
 
 fun main() {
   val input = readInput("/day03/input")
-  val testinput = readInput("/day03/testinput")
+  val testinput = readInput("/day03/input")
+
+  println(part1(input))
+  println(part2(testinput))
 }
 
