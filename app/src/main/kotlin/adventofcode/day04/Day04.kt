ef12dd11f9d857bb4bc09d83bbeb8be35d789845
Stefan Hering <shering@spotify.com>
2022-12-04 10:49:44 +0100

    Day 4

--- app/src/main/kotlin/adventofcode/day04/Day04.kt
+++ app/src/main/kotlin/adventofcode/day04/Day04.kt
@@ -4,9 +4,35 @@ fun readInput(file: String) = Unit.javaClass.getResource(file)
     .readText()
     .lines()
     .filter { it.isNotEmpty() }
+    .map {
+      it.split(",")
+          .map {
+            it.split("-").map { it.toInt() }
+          }
+    }
+    .map {
+      it[0][0]..it[0][1] to it[1][0]..it[1][1]
+    }
+
+typealias SectionAssignment = Pair<IntRange, IntRange>
+
+fun part1(assignments: List<SectionAssignment>) =
+    assignments.count { (elf1, elf2) ->
+      elf1.first >= elf2.first && elf1.last <= elf2.last ||
+          elf1.first <= elf2.first && elf1.last >= elf2.last
+    }
+
+fun part2(assignments: List<SectionAssignment>) =
+    assignments.count { (elf1, elf2) ->
+      elf1.last >= elf2.first && elf1.first <= elf2.last ||
+          elf1.last <= elf2.first && elf1.first >= elf2.last
+    }
 
 fun main() {
   val input = readInput("/day04/input")
   val testinput = readInput("/day04/testinput")
+
+  println(part1(input))
+  println(part2(input))
 }
 
