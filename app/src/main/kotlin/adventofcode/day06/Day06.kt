58291c8cdd9d8bf478b5a773fe534275715a0d48
Stefan Hering <shering@spotify.com>
2022-12-07 08:43:16 +0100

    Day 6

--- app/src/main/kotlin/adventofcode/day06/Day06.kt
+++ app/src/main/kotlin/adventofcode/day06/Day06.kt
@@ -2,11 +2,19 @@ package adventofcode.day06
 
 fun readInput(file: String) = Unit.javaClass.getResource(file)
     .readText()
-    .lines()
-    .filter { it.isNotEmpty() }
+    .trim()
+
+fun part1(signal: String, size: Int = 4) =
+    signal.windowed(size = size)
+      .indexOfFirst {
+        it.chars().distinct().count() == size.toLong()
+      } + size
 
 fun main() {
   val input = readInput("/day06/input")
   val testinput = readInput("/day06/testinput")
+
+  println(part1(input))
+  println(part1(input, 14))
 }
 
